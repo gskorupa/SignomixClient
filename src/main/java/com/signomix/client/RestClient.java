dfecf180e1d9537847a6f8701b09cb070eb2d9e1
g.skorupa@gmail.com <>
2020-04-14 00:06:14 +0200

    test

--- src/main/java/com/signomix/client/RestClient.java
+++ src/main/java/com/signomix/client/RestClient.java
@@ -1,16 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.signomix.client;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.io.UnsupportedEncodingException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -46,6 +44,8 @@ import org.apache.http.message.BasicNameValuePair;
  * @author greg
  */
 public class RestClient {
+    
+    Properties props;
 
     public static void main(String[] args) {
         String url = null;
@@ -54,12 +54,16 @@ public class RestClient {
         String authKey = null;
         int interval = 0;
         String provider = "";
+        String line = null;
         RestClient restClient = new RestClient();
+        restClient.readProperties();
+        //prop.setProperty("db.url", "localhost");
 
         // create Options object
         Options options = new Options();
         options.addOption("u", "url", true, "Signomix service URL");
         options.addOption("f", "file", true, "data file location");
+        options.addOption("l", "line", true, "commandline params");
         options.addOption("e", "eui", true, "device EUI");
         options.addOption("a", "auth", true, "device authorization key");
         options.addOption("i", "interval", true, "interval between transmissions");
@@ -68,15 +72,28 @@ public class RestClient {
         try {
             CommandLine cmd = parser.parse(options, args);
             url = cmd.getOptionValue("u");
+            if (null == url) {
+                url = restClient.props.getProperty("url");
+            }
             fileLocation = cmd.getOptionValue("f");
             deviceEUI = cmd.getOptionValue("e");
+            if (null == deviceEUI) {
+                deviceEUI = restClient.props.getProperty("eui");
+            }
             authKey = cmd.getOptionValue("a");
+            if (null == authKey) {
+                authKey = restClient.props.getProperty("auth.key");
+            }
             provider = cmd.getOptionValue("p");
+            if (null == provider) {
+                provider = restClient.props.getProperty("integration.type");
+            }
             try {
                 interval = Integer.parseInt(cmd.getOptionValue("i"));
             } catch (NumberFormatException | NullPointerException e) {
             }
-            if (null == url || null == authKey || null == fileLocation) {
+            line = cmd.getOptionValue("l");
+            if (null == url || null == authKey || (null == fileLocation && null == line)) {
                 restClient.printHelp(options);
                 System.exit(2);
             }
@@ -90,16 +107,31 @@ public class RestClient {
         System.out.println("FILE:" + fileLocation);
         System.out.println("EUI:" + deviceEUI);
         System.out.println("AUTH:" + authKey);
+
+        // properties
+        restClient.props.setProperty("url", url);
+        restClient.props.setProperty("auth.key", authKey);
+        restClient.props.setProperty("eui", deviceEUI);
+        if(null==provider || provider.isEmpty()){
+            provider="REST";
+        }
+        restClient.props.setProperty("integration.type", provider);
+
         if ("TTN".equalsIgnoreCase(provider)) {
             restClient.sendAsJson(url, authKey, fileLocation);
+            restClient.saveProperties();
         } else if ("REST".equalsIgnoreCase(provider) || provider.isEmpty()) {
-            if(null==deviceEUI){
+            if (null == deviceEUI) {
                 restClient.printHelp(options);
                 System.exit(2);
             }
             System.out.println("INTERVAL:" + interval + " seconds");
-            restClient.sendAsGeneric(url, authKey, deviceEUI, fileLocation, interval);
-
+            if (null != fileLocation) {
+                restClient.sendAsGeneric(url, authKey, deviceEUI, fileLocation, interval);
+            } else {
+                restClient.sendAsGeneric(url, authKey, deviceEUI, line);
+            }
+            restClient.saveProperties();
         } else {
             System.out.println("Provider name " + provider + " not valid");
         }
@@ -129,7 +161,7 @@ public class RestClient {
             if (record.get(0).startsWith("#") && null == headers) {
                 // first commented line must be header
                 lineNumber--;
-                headers = values = new String[record.size()];
+                headers = new String[record.size()];
                 for (int i = 0; i < record.size(); i++) {
                     if (i == 0) {
                         headers[i] = record.get(i).substring(1);
@@ -165,7 +197,24 @@ public class RestClient {
         }
     }
 
-    private void sendAsJson(String url, String authKey,String fileLocation) {
+    private void sendAsGeneric(String url, String authKey, String deviceEUI, String line) {
+        String[] headers = null;
+        String[] values = null;
+        String[] parts = line.split("=");
+        System.out.println("LINE:"+line);
+        if (parts.length >1 ) {
+            headers = parts[0].split(",");
+            values = parts[1].split(",");
+            props.setProperty("data.names",parts[0]);
+        }else if(parts.length==1){
+            headers = props.getProperty("data.names").split(",");
+            values = parts[0].split(",");
+        }
+        long lineNumber = 0;
+        send(url, authKey, deviceEUI, headers, values, lineNumber);
+    }
+
+    private void sendAsJson(String url, String authKey, String fileLocation) {
         File file;
         FileReader fr;
         try {
@@ -178,19 +227,11 @@ public class RestClient {
             while ((line = br.readLine()) != null) {
                 json = json.concat(line);
             }
-            send(url,authKey,json);
+            send(url, authKey, json);
         } catch (IOException ex) {
             ex.printStackTrace();
             System.exit(7);
         }
-    }
-
-    private String[] getHeaders(String line) {
-        String l = line;
-        while (l.startsWith("#")) {
-            l = l.substring(1);
-        }
-        return l.split(",");
     }
 
     private void send(String url, String authKey, String eui, String[] names, String[] values, long lineNumber) {
@@ -245,7 +286,7 @@ public class RestClient {
             System.exit(5);
         }
     }
-    
+
     private void send(String url, String authKey, String json) {
         String apiPath = "api/ttn";
         if (!url.endsWith("/")) {
@@ -316,5 +357,26 @@ public class RestClient {
         } catch (IOException e) {
         }
         return "";
+    }
+
+    private void saveProperties() {
+        String usersHomeDir = System.getProperty("user.home");
+        String propertyFilePath = usersHomeDir + File.separator + ".SignomixClient.properties";
+        try (OutputStream output = new FileOutputStream(propertyFilePath)) {
+            props.store(output, null);
+        } catch (IOException io) {
+            io.printStackTrace();
+        }
+    }
+
+    private void readProperties() {
+        props = new Properties();
+        String usersHomeDir = System.getProperty("user.home");
+        String propertyFilePath = usersHomeDir + File.separator + ".SignomixClient.properties";
+        try (InputStream input = new FileInputStream(propertyFilePath)) {
+            props.load(input);
+        } catch (IOException io) {
+            //io.printStackTrace();
+        }
     }
 }
